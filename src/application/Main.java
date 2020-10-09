af65130102f82a9ca989b7c4b7e1202207e33955
PhTL19 <phatk96lhp@gmail.com>
2020-10-09 12:42:26 +0700

    Make new mainDictionary for all controllers & app

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,6 @@
 package application;
 
+import code.Dictionary;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -7,6 +8,8 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    public static Dictionary mainDictionary = new Dictionary();
+
     @Override
     public void start(Stage primaryStage) throws Exception{
 //        window = primaryStage;
