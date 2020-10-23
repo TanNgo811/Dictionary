b7ce6799a05c7c10ae73945d0744421e19f52517
PhTL19 <phatk96lhp@gmail.com>
2020-10-24 00:36:24 +0700

    add Drag + About + fix export method

--- src/application/controller_with_db/MainController_db.java
+++ src/application/controller_with_db/MainController_db.java
@@ -1,5 +1,6 @@
 package application.controller_with_db;
 
+import application.Main;
 import code.DictionaryManagement;
 import code.Word;
 import javafx.event.ActionEvent;
@@ -11,7 +12,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.SVGPath;
 import javafx.stage.Stage;
 import javazoom.jl.decoder.JavaLayerException;
@@ -28,6 +29,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import static application.Main.mainDictionary;
+import static application.Main.searchedWords;
 import static application.controller.NotiWindow.openAlertWindow;
 import static tools.Database.deleteFromDatabase;
 import static tools.Database.insertFromDatabase;
@@ -83,13 +85,15 @@ public class MainController_db implements Initializable {
     public Button btnMinimize;
 
     @FXML
-    public AnchorPane apMain;
+    public Rectangle recMain;
+
+    @FXML
+    public Button btnAbout;
 
 //    Dictionary mainDictionary = new Dictionary();
 //    DictionaryManagement management = new DictionaryManagement();
-    ArrayList<Word> searchedWords = new ArrayList<Word>();
 
-    public MainController_db(){
+    public MainController_db() {
 
     }
 
@@ -98,12 +102,12 @@ public class MainController_db implements Initializable {
 
     final String HOVERED_BUTTON_STYLE2 = "-fx-background-color: #ff0800; -fx-background-radius: 10px; -fx-text-fill: white";
     final String IDLE_BUTTON_STYLE2 = "-fx-background-color: transparent; -fx-text-fill: #ff0800; -fx-border-color: #ff0800; -fx-border-radius: 10px";
-    double x = 0, y = 0;
+    double x, y;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         lbWord.setText("OpenSource E-V Dictionary");
-        taMeaning.setText("Main developer: NGO TIEN TAN - 18021123\n & LE TIEN PHAT - 18020993 \nWord & translation source from Oxford Basic Dictionary & Google Translate Source");
+        taMeaning.setText("Main developer: \nNGO TIEN TAN - 18021123\n & LE TIEN PHAT - 18020993 \nWord & translation source from http://informatik.uni-lebzig.de/~duc/Dict & Google Translate Source");
 
         mainDictionary.words.clear();
 //        DictionaryManagement.insertFromFile(mainDictionary);
@@ -143,7 +147,7 @@ public class MainController_db implements Initializable {
         lvWords.setOnMouseClicked(event -> {
             String searchedWord = lvWords.getSelectionModel().getSelectedItem();
             try {
-                if (!searchedWord.equals("")||searchedWord != null) {
+                if (!searchedWord.equals("") || searchedWord != null) {
                     System.out.println("Clicked Word: " + searchedWord);
                     tfSearchedWord.setText(searchedWord);
                     Word clickedWord = DictionarySearcher.exactSearcherWord(searchedWord, mainDictionary.words);
@@ -152,12 +156,12 @@ public class MainController_db implements Initializable {
                     searchedWords.add(clickedWord);
                 }
             } catch (NullPointerException e) {
-                    System.out.println("Nothing Here!!!");
+                System.out.println("Nothing Here!!!");
             }
 
         });
 
-        btnDelete.setOnMouseClicked(event ->{
+        btnDelete.setOnMouseClicked(event -> {
             String word = lbWord.getText();
             if (!word.equals("")) {
                 DictionaryManagement.deleteWords(mainDictionary, word);
@@ -191,41 +195,35 @@ public class MainController_db implements Initializable {
 //
 //        GoogleSVG.setOnMouseEntered(e -> {GoogleSVG.setStyle("-fx-fill: white");});
 //        GoogleSVG.setOnMouseExited(e -> {GoogleSVG.setStyle("-fx-fill: #ff0800");});
+        btnAbout.setOnMouseClicked(event -> {
+            lbWord.setText("OpenSource E-V Dictionary");
+            taMeaning.setText("Main developer: \nNGO TIEN TAN - 18021123\n & LE TIEN PHAT - 18020993 \nWord & translation source from http://informatik.uni-lebzig.de/~duc/Dict & Google Translate Source");
+        });
 
         btnMinimize.setOnMouseClicked(event -> {
-//            ((Stage)((Button)event.getSource()).getScene().getWindow()).setIconified(true);
-            Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-            stage.setIconified(true);
+            ((Stage) ((Button) event.getSource()).getScene().getWindow()).setIconified(true);
         });
 
         btnExit.setOnMouseClicked(event -> {
-            Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-            stage.close();
+            ((Stage) ((Button) event.getSource()).getScene().getWindow()).close();
         });
 
+        recMain.setOnMousePressed(event -> {
+            x = event.getSceneX();
+            y = event.getSceneY();
+        });
 
-        try {
-            apMain.setOnMousePressed(event -> {
-                x = event.getSceneY();
-                y = event.getSceneY();
-            });
-
-            apMain.setOnMouseDragged(event -> {
-                Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-                stage.setX(event.getScreenX()-x);
-                stage.setY(event.getScreenY()-y);
-            });
-        } catch (Exception e) {
-
-        }
-
-
+        recMain.setOnMouseDragged(event -> {
+            Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            stage.setX(event.getScreenX() - x);
+            stage.setY(event.getScreenY() - y);
+        });
     }
 
 
     public void updateWordList(ArrayList<Word> updatedArray) {
         lvWords.getItems().clear();
-        for (Word i : updatedArray){
+        for (Word i : updatedArray) {
             lvWords.getItems().add(i.getWordTarget());
         }
     }
@@ -303,18 +301,17 @@ public class MainController_db implements Initializable {
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(getClass().getResource("../fxml/ExportFile.fxml"));
             Parent editViewParent = loader.load();
-            ExportController exportController = loader.getController();
-            exportController.setLVSearchWords(getSearchWords());
+//            ExportController exportController = loader.getController();
+//            exportController.setLVSearchWords(getSearchWords());
 //        Parent editViewParent = FXMLLoader.load(getClass().getResource("EditWord.fxml"));
             Scene scene = new Scene(editViewParent);
             stage.setScene(scene);
         }
     }
 
-    public void handleTextToSpeed(ActionEvent actionEvent)
-    {
+    public void handleTextToSpeed(ActionEvent actionEvent) {
         System.out.println("Playing Text to Speech");
-        String word =lbWord.getText();
+        String word = lbWord.getText();
 
         btnSound = (Button) actionEvent.getSource();
         String targetLang = btnSound.getId();
@@ -352,5 +349,4 @@ public class MainController_db implements Initializable {
             Logger.getLogger(MainController_db.class.getName()).log(Level.SEVERE, null, jle);
         }
     }
-
 }
